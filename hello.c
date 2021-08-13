ac431ce4be5d67a8fd9fb81c13c91a3c80b4228c
kimwon1992 <kjwgogogo@gmail.com>
2021-08-13 15:42:05 +0900

    conversion specifier

--- hello.c
+++ hello.c
@@ -2,7 +2,8 @@
 
 int main (void)
 {
-    printf("hello world \n");
-    printf("이름 : 김원");
+    printf("My age: %d \n", 20);
+    printf("%d is my point \n", 100);
+    printf("Good \nmorning \neverybody\n");
     return 0;
 }
