6cbd2baf700be48b07d43691cf23ee1962bf82b3
kimwon1992 <kjwgogogo@gmail.com>
2021-08-23 16:59:38 +0900

    -m알고리즘 1

--- hello.c
+++ hello.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int main (void)
-{
-    printf("제 이름은 홍길동입니다.\n");
-    printf("제 나이는 %d살이구요.\n", 20);
-    printf("제가 사는 곳의 번지수는 %d-%d입니다.\n",123,456);
-
-    printf("%dx%d=%d\n",4,5,4*5);
-    printf("%dx%d=%d\n",7,9,7*9);
-    int num;
-    num=20;
-    printf("%d\n", num);
-
-    int num1=3;
-    int num2=4;
-    int result=num1+num2;
-    printf("덧셈결과:%d\n",result);
-    printf("%d+%d=%d \n",num1, num2, result);
-    printf("%d와 %d의 합은 %d입니다.\n", num1,num2,result);    
+int main (void){
+	int i, j, min, index, temp;
+	int array[10] = {1,10,5,8,7,6,4,3,2,9};
+	for(i =0; i < 10; i++){
+	min = 9999;
+	for(j=i; j<10; j++){
+	   if(min > array[j]){
+	      min = array[j];
+	      index=j;
+	   }
+	}
+	temp = array[i];
+	array[i] = array[index];
+	array[index] = temp;
+	}
+        for(i=0; i <10; i++){
+	   printf("%d", array[i]);
+	}
     return 0;
 }
